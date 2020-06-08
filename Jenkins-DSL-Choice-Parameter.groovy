74cb00f823bcb4fd020c0d0c6ae3d490554e2799
rmaheshvarma <53750144+rmaheshvarma@users.noreply.github.com>
2020-06-08 13:43:51 +0530

    Jenkins DSL choice parameter declaration
    Jenkins DSL choice parameter declaration

---
+++ Jenkins-DSL-Choice-Parameter.groovy
@@ -0,0 +1,17 @@
+
+job("test-dsl-db") {
+       description 'Setup git repo copy to s3 '
+    wrappers {
+      timestamps()
+      
+      }
+    
+    logRotator {
+      daysToKeep(90)
+   }
+      
+     parameters {
+        choiceParam('ACCOUNT', ['root-7865457'],'Select the AWS Account')
+		    choiceParam('REGION', ['us-east-1','us-west-1','us-est-2','us-west-2'],'Select the AWS REGION')
+		    choiceParam('ENV_TYPE', ['dev','stage','prod'],'Select the ENV_TYPE')
+	    	choiceParam('TENANT', ['test-rnd-pm'],'Select the TENANT')
